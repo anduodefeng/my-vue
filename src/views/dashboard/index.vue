ed33f664b3508309f7b2ab7ea9018a7b32e9e298
maze <maze1992@126.com>
2022-01-27 19:42:37 +0800

    首页规划

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">欢迎你！！</div>
+    <div style="border:1px solid black; width:1400px; height:50px">总资产、昨日盈亏</div>
+    <div style="border:1px solid black; width:600px; height:300px;float:left;">总资产构成=现金+货币+固收+积极增值</div>
+    <div style="border:1px solid black; width:600px; height:300px;float:left;">总资产变化折线图</div>
+    <div style="border:1px solid black; width:600px; height:300px;float:left;">指数基金收益率变化折线图</div>
+    <div style="border:1px solid black; width:600px; height:300px;float:left;">主动基金收益率变化折线图</div>
+    <div style="border:1px solid black; width:600px; height:300px;float:left;">稳健组合收益率变化折线图</div>
+    <div style="border:1px solid black; width:600px; height:300px;float:left;">积极增值组合收益率变化折线图</div>
   </div>
 </template>
 
